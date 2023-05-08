be77d1c51725842fdfe77866418c7f3db945c8c2
Soumya Das <56791208+soumya974@users.noreply.github.com>
2023-05-08 20:40:39 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,17 @@
-import 
+import tensorflow
+
+# Define the input shape
+input_shape = (None, None, 3)
+# Define the FOTS network architecture
+inputs = tensorflow.keras.layers.Input(shape=input_shape)
+outputs = tensorflow.keras.layers.Dense(num_classes, activation='softmax')(x)
+model = tensorflow.keras.modelsModel(inputs, outputs)
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# Create data generators for training and validation sets
+train_datagen = tensorflow.keras.preprocessing.image.ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+train_generator = train_datagen.flow_from_directory('path/to/training/dataset', target_size=input_shape[:2], batch_size=batch_size, class_mode='categorical')
+validation_datagen  = tensorflow.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+validation_generator  = validation_datagen.flow_from_directory('path/to/training/dataset', target_size=input_shape[:2], batch_size=batch_size, class_mode='categorical')
+# Train the model
+model.fit(train_generator, steps_per_epoch=steps_per_epoch, epochs=num_epochs, validation_data=validation_generator, validation_steps=validation_steps)
