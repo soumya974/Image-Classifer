fe1eb975100e5d082cd021a772069ccc5d5a3779
Soumya Das <56791208+soumya974@users.noreply.github.com>
2023-05-08 20:43:31 +0530

    Update script.py

--- script.py
+++ script.py
@@ -7,12 +7,18 @@ input_shape = (None, None, 3)
 inputs = tensorflow.keras.layers.Input(shape=input_shape)
 outputs = tensorflow.keras.layers.Dense(num_classes, activation='softmax')(x)
 model = tensorflow.keras.modelsModel(inputs, outputs)
+# Define the loss function and metrics for training
+loss_fn = tensorflow.keras.losses.CategoricalCrossentropy()
+metric_fn = tensorflow.keras.metrics.CategoricalAccuracy()
+# Define the optimizer and learning rate schedule
+optimizer = tensorflow.keras.optimizers.Adam(learning_rate=0.001)
+lr_schedule = tensorflow.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=0.001, decay_steps=10000, decay_rate=0.9)
 # Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=optimizer, loss=loss_fn, metrics=[metric_fn])
 # Create data generators for training and validation sets
-train_datagen = tensorflow.keras.preprocessing.image.ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+train_datagen = tensorflow.keras.preprocessing.image.ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True, preprocessing_function=tensorflow.keras.applications.resnet50.preprocess_input)
 train_generator = train_datagen.flow_from_directory('path/to/training/dataset', target_size=input_shape[:2], batch_size=batch_size, class_mode='categorical')
-validation_datagen  = tensorflow.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+validation_datagen  = tensorflow.keras.preprocessing.image.ImageDataGenerator(rescale=1./255, preprocessing_function=tensorflow.keras.applications.resnet50.preprocess_input)
 validation_generator  = validation_datagen.flow_from_directory('path/to/training/dataset', target_size=input_shape[:2], batch_size=batch_size, class_mode='categorical')
 # Train the model
 model.fit(train_generator, steps_per_epoch=steps_per_epoch, epochs=num_epochs, validation_data=validation_generator, validation_steps=validation_steps)
